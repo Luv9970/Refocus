0c61400c213a3869114007bb6dfa1d2482bef246
Luv <ir>
2025-04-16 22:35:48 +0530

    fourth commit

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Button from './Button'
 
-const Product = ({val}) => {
+const Product = ({val, mover,count}) => {
 
   return (
-    <div className="w-full py-20 text-white">
-        <div className="max-w-screen-xl mx-auto flex items-center justify-between ">
+    <div className="w-full py-20 h-[23rem] text-white">
+        <div onMouseEnter={()=>{mover(count)}} className="max-w-screen-xl mx-auto flex items-center justify-between ">
             <h1 className="text-6xl capitalise font-medium">{val.title}</h1>
             <div className="dets w-1/3">
                 <p className="mb-6">{val.description}</p>
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,48 +1,92 @@
-import React from 'react'
-import Product from './Product'
+import React, { useState } from "react";
+import Product from "./Product";
+import { motion } from "framer-motion";
 
 const Products = () => {
+  var products = [
+    {
+      title: "arqitel",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus.",
+      live: true,
+      case: false,
+    },
+    {
+      title: "TTR",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus.",
+      live: true,
+      case: true,
+    },
+    {
+      title: "YIR 2022",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus.",
+      live: true,
+      case: false,
+    },
+    {
+      title: "yahoo",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus.",
+      live: true,
+      case: true,
+    },
+    {
+      title: "rainfall",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus.",
+      live: true,
+      case: true,
+    },
+  ];
 
-    var products = [
-        {
-            title:"arqitel" , 
-            description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus." , 
-            live: true , 
-            case: false
-        },  
-        {
-            title:"TTR" , 
-            description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus." , 
-            live: true , 
-            case: true
-        },
-        {
-            title:"YIR 2022" , 
-            description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus." , 
-            live: true , 
-            case: false
-        },
-        {
-            title:"yahoo" , 
-            description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus." , 
-            live: true , 
-            case: true
-        },
-        {
-            title:"rainfall" , 
-            description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, hic saepe? Neque officia veritatis, quia alias odio iusto voluptate temporibus." , 
-            live: true , 
-            case: true
-        }
-    ]
+  const [pos, setpos] = useState(0);
+  const mover = (val)=>{
+    setpos(val*23)
+  }
 
   return (
-    <div className="mt-24">
-        {products.map((val , index)=>(
-            <Product val={val} key={index}/>
-        ))}
+    <div className="mt-24 relative">
+      {products.map((val, index) => (
+        <Product val={val} key={index} mover={mover} count={index} />
+      ))}
+      <div className="absolute top-0 pointer-events-none w-full h-full">
+        <motion.div
+          initial={{ y: pos, x: "-50%" }}
+          animate={{ y: pos + 'rem' }}
+          transition={{ease:[0.76 , 0 , 0.24 , 1] , duration:.6}}
+          className="window absolute left-[45%] w-[32rem] h-[23rem]  bg-white overflow-hidden"
+        >
+          <motion.div 
+                animate={{y: -pos+ `rem`}} 
+                transition={{ease:[0.76 , 0 , 0.24 , 1] , duration:.6}}
+                className="window w-full h-full  bg-sky-100"> 
+        </motion.div>
+          <motion.div 
+                animate={{y: -pos+ `rem`}} 
+                transition={{ease:[0.76 , 0 , 0.24 , 1] , duration:.5}}
+                className="window w-full h-full  bg-sky-200"> 
+        </motion.div>
+          <motion.div 
+                animate={{y: -pos+ `rem`}} 
+                transition={{ease:[0.76 , 0 , 0.24 , 1] , duration:.5}}
+                className="window w-full h-full  bg-sky-300"> 
+        </motion.div>
+          <motion.div 
+                animate={{y: -pos+ `rem`}} 
+                transition={{ease:[0.76 , 0 , 0.24 , 1] , duration:.5}}
+                className="window w-full h-full  bg-sky-400"> 
+        </motion.div>
+          <motion.div 
+                animate={{y: -pos+ `rem`}} 
+                transition={{ease:[0.76 , 0 , 0.24 , 1] , duration:.5}}
+                className="window w-full h-full  bg-sky-500"> 
+        </motion.div>
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
