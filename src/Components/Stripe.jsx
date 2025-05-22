d4eba829cd1331b6dadd24a10998100590d08a19
Luv <ir>
2025-05-22 08:16:35 +0530

    Fifth Commit

--- src/Components/Stripe.jsx
+++ src/Components/Stripe.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+
 const Stripe = ({val}) => {
+
+    var data = [
+        {url:"image1" , number:48},
+        {url:"image2" , number:2},
+        {url:"image3" , number:11},
+        {url:"image4" , number:48},
+        {url:"image5" , number:2},
+        {url:"image1" , number:11},
+    ]
+
   return (
-    <div className="w-[16.66%] px-4 border-t-[1.2px] border-b-[1.2px] border-r-[1.2px] border-zinc-600 flex justify-between items-center">
-        <img src={val.url}/>
+    <div className="w-[16.66%]  px-4 border-t-[1.2px] border-b-[1.2px] border-r-[1.2px] border-zinc-600 flex justify-between items-center">
+        <img src={val.url} className='w-28 h-24'/>
         <span className="font-semibold">{val.number}</span>
     </div>
   )
--- src/Components/Stripes.jsx
+++ src/Components/Stripes.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import Stripe from './Stripe'
+import image1 from '../assets/image1.png'
+import image2 from '../assets/image2.png'
+import image3 from '../assets/image3.png'
+import image4 from '../assets/image4.png'
+import image5 from '../assets/image5.png'
 
 const Stripes = () => {
 
-    var data = [
-        {url:"https://cdn-eahjn.nitrocdn.com/ChEvwayTHZmZJUAdsUNMLXuXZdBprFoQ/assets/images/optimized/rev-239bc41/www.spinxdigital.com/app/uploads/2022/11/TCE.svg" , number:48},
-        {url:"https://cdn-eahjn.nitrocdn.com/ChEvwayTHZmZJUAdsUNMLXuXZdBprFoQ/assets/images/optimized/rev-239bc41/www.spinxdigital.com/app/uploads/2022/11/RedBarn.svg" , number:2},
-        {url:"https://cdn-eahjn.nitrocdn.com/ChEvwayTHZmZJUAdsUNMLXuXZdBprFoQ/assets/images/optimized/rev-239bc41/www.spinxdigital.com/app/uploads/2022/11/Lipsmacker2.svg" , number:11},
-        {url:"https://cdn-eahjn.nitrocdn.com/ChEvwayTHZmZJUAdsUNMLXuXZdBprFoQ/assets/images/optimized/rev-239bc41/www.spinxdigital.com/app/uploads/2022/11/RedBarn.svg" , number:48},
-        {url:"https://cdn-eahjn.nitrocdn.com/ChEvwayTHZmZJUAdsUNMLXuXZdBprFoQ/assets/images/optimized/rev-239bc41/www.spinxdigital.com/app/uploads/2022/11/QE-white.svg" , number:2},
-        {url:"https://cdn-eahjn.nitrocdn.com/ChEvwayTHZmZJUAdsUNMLXuXZdBprFoQ/assets/images/optimized/rev-239bc41/www.spinxdigital.com/app/uploads/2022/11/PhysiciansFormula-1.svg" , number:11},
-    ]
+    // var data = [
+    //     {url:"image1" , number:48},
+    //     {url:"image2" , number:2},
+    //     {url:"image3" , number:11},
+    //     {url:"image4" , number:48},
+    //     {url:"image5" , number:2},
+    //     {url:"image1" , number:11},
+    // ]
 
   return (
     <div className="flex items-center mt-10">
-        {data.map((elem , index)=>(
+        {/* {data.map((elem , index)=>(
             <Stripe key={index} val={elem} />
-            ))}
+            ))} */}
+
+        <Stripe val={{url:image1 , number:48}} />
+        <Stripe val={{url:image2 , number:48}} />
+        <Stripe val={{url:image3 , number:48}} />
+        <Stripe val={{url:image4 , number:48}} />
+        <Stripe val={{url:image5 , number:48}} />
+        <Stripe val={{url:image1 , number:48}} />
     </div>
   )
 }
